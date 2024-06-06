73f7fcbfbd725f0e271dd6daefe71a99b8187047
Martina Combi <m.combi7@campus.unimib.it>
2024-06-06 18:25:35 +0200

    format document pagine

--- 2_destinazioni/Carosello pianeti/carosello.js
+++ 2_destinazioni/Carosello pianeti/carosello.js
@@ -4,33 +4,33 @@ const nav_prev = $('.slider-nav.prev');
 
 let selected_item = 0;
 
-function setItemSlider(index){
+function setItemSlider(index) {
     selected_item = index;
 
-    slider_items.each(function(idx){
+    slider_items.each(function (idx) {
         let offset = idx - selected_item;
-        if(offset < 0) offset += slider_items.length;
+        if (offset < 0) offset += slider_items.length;
 
         for (let i = 0; i < slider_items.length + 1; i++) {
 
             $(this)
-            .removeClass(`item-${i}`)
-            .addClass(`item-${offset+1}`)
+                .removeClass(`item-${i}`)
+                .addClass(`item-${offset + 1}`)
         }
     })
 
 }
 
-slider_items.click(function (){
+slider_items.click(function () {
     setItemSlider($(this).index());
 });
 
-nav_next.click(function(){
-    selected_item = selected_item < slider_items.length - 1 ? ++selected_item:0;
+nav_next.click(function () {
+    selected_item = selected_item < slider_items.length - 1 ? ++selected_item : 0;
     setItemSlider(selected_item);
 })
 
-nav_prev.click(function(){
-    selected_item = selected_item >= 1 ? --selected_item: slider_items.length -1;
+nav_prev.click(function () {
+    selected_item = selected_item >= 1 ? --selected_item : slider_items.length - 1;
     setItemSlider(selected_item);
 })
